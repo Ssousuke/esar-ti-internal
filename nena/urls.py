f3f474e5d5be847f8f60662f1e94ec40f8104316
Wesley Farias <wesleysousukee@gmail.com>
2022-05-16 16:48:44 -0300

    adiciona dashboard

--- nena/urls.py
+++ nena/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('equipamentos/', include('apps.equipamentos.urls', namespace='equipments')),
     path('rede/', include('apps.rede.urls', namespace='network')),
     path('', include('apps.accounts.urls', namespace='accounts')),
+    path('dashboard/', include('apps.dashboard.urls', namespace='dashboard')),
 ]
