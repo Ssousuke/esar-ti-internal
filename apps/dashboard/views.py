15f75aa0ea5445c6cd13d85bee06fbbea4b4ce12
Wesley Farias <wesleysousukee@gmail.com>
2022-06-01 06:18:51 -0300

    adiciona queryset

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from apps.equipamentos.models import Equipments
+from apps.rede.models import Networks
 
 
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    equipment_count = Equipments.objects.all().count()
+    equipment_network_count = Networks.objects.all().count()
+    context = {'equipment_count': equipment_count,
+               'equipment_network_count': equipment_network_count}
+    return render(request, 'dashboard/dashboard.html', context)
