342994e0734c3a77bc1c919f3be740eb7a526e51
Wesley Farias <wesleysousukee@gmail.com>
2022-06-13 15:04:55 -0300

    adiciona admin

--- apps/rede/admin.py
+++ apps/rede/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from apps.rede.models import Department, Networks
 
-# Register your models here.
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'name'
+    ]
+    list_display_links = [
+        'id',
+        'name'
+    ]
+
+
+@admin.register(Networks)
+class NetworkAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'department',
+        'equipment',
+        'ipv4_or_ipv6',
+        'type'
+    ]
+    list_display_links = [
+        'id',
+        'department',
+        'equipment',
+        'ipv4_or_ipv6',
+        'type'
+    ]
